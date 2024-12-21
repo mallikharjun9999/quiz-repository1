9098a38e33f0cb41bd3cd1429db43479e9ea629c
mallikharjun9999 <penumallikharjun@gmail.com>
2024-12-21 09:46:00 +0530

    Delete quize directory

--- quize/src/main/java/com/conceptile/quize/repositories/QuizResultRepository.java
+++
@@ -1,9 +0,0 @@
-package com.conceptile.quize.repositories;
-
-import com.conceptile.quize.entities.QuizResult;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuizResultRepository extends JpaRepository<QuizResult, Long> {
-}
